2e9b47764bc12854e94d744a26c76b079a4efb79
Max S Price <78626033+maxsprice@users.noreply.github.com>
2022-10-24 11:40:03 -0600

    Update NBAScrapeCode.py

--- NBAScrapeCode.py
+++ NBAScrapeCode.py
@@ -1,32 +1,35 @@
+#Import the required packages
 from bs4 import BeautifulSoup
 import requests
 import re
 import pandas as pd
 import numpy as np
 
+#Select the url and commit it to a usable format using BeautifulSoup
 url = 'https://www.basketball-reference.com/draft/'
 r = requests.get(url)
 r.status_code
 soup = BeautifulSoup(r.text)
 
+#Join the end of the link for each draft year to original url
 links = []
 for link in soup.find_all('a'):
     href = link.get('href')
     if 'draft/' in href:
         links.append(href)
-# for i in range(1,len(links)):
-#     links[i] = ''.join(str(links[i]))
 links = ',https://www.basketball-reference.com'.join(links)
 links = links.split(",")
 links = links[1:77]
 links
 
+#Writes a function that will take the chosen info and append it to a list
 def directory_item(bs, tag, search, direct, col):
     item = bs.find_all(tag, {"data-stat": search})
     for info in item:
         direct[col].append(info.get_text())
-        
-        directory = {'Pick':[], 'Team':[], 'Player':[], 'College':[], 'Yrs Played':[], 'Min Per Game':[], 'Points Avg.':[], 'Rebounds Avg.':[], 'Assists Avg.':[]}
+      
+#Creates a directory to be appended to then loops through all of the different drafts' urls
+directory = {'Pick':[], 'Team':[], 'Player':[], 'College':[], 'Yrs Played':[], 'Min Per Game':[], 'Points Avg.':[], 'Rebounds Avg.':[], 'Assists Avg.':[]}
 for url in links:
     r = requests.get(url)
     if r.status_code == 200:
@@ -40,6 +43,7 @@ for url in links:
         directory_item(bs, "td", "pts_per_g",directory, 'Points Avg.')
         directory_item(bs, "td", "trb_per_g",directory, 'Rebounds Avg.')
         directory_item(bs, "td", "ast_per_g",directory, 'Assists Avg.')
+        
+#Creates a dataframe then exports it as a csv
 Playerdf = pd.DataFrame(directory)
-
 Playerdf.to_csv('DraftData.csv')
